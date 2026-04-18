d23f417d0b23cd9ebe3237b3aaa425bf9bd8d4e3
Chase Davenport <luigiandyoshi888@gmail.com>
2026-04-18 07:47:12 -0400

    fix

--- Views/SettingsPage.xaml.cs
+++ Views/SettingsPage.xaml.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading.Tasks;
 using GelBox.ViewModels;
+using Windows.UI.Xaml;
 
 namespace GelBox.Views
 {
@@ -24,6 +25,11 @@ namespace GelBox.Views
         protected override async Task InitializePageAsync(object parameter)
         {
             await TypedViewModel.InitializeAsync();
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService?.GoBack();
         }
     }
 }
